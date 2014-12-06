eb89a2615c4a9604932b99b74b35c3577d3558c0
rjjkathy <rjjkathy@hotmail.com>
2014-12-05 20:51:58 -0500

    single calculation bug fix

--- SingleIndirectBlock.java
+++ SingleIndirectBlock.java
@@ -1,61 +1,69 @@
 import java.util.ArrayList;
 
 /**
- * Single Indirect Block
- * Each Single Indirect Block contains max 100 direct blocks
+ * Single Indirect Block Each Single Indirect Block contains max 100 direct
+ * blocks
+ * 
  * @author Jingjing&Ning
- *
+ * 
  */
 public class SingleIndirectBlock {
 
 	// File name
 	private String fileName = "level1_indirection.txt";
-		
+
 	// Stores 100 SingleIndirectBlocks
 	private ArrayList<DirectBlock> directBlocks = new ArrayList<DirectBlock>();
-	
-	public SingleIndirectBlock()
-	{
-      init();
+
+	public SingleIndirectBlock() {
+		init();
 	}
-	
-	private void init()
-	{
-		for(int i = 0; i < 100; i++)
-		{
+
+	private void init() {
+		for (int i = 0; i < 100; i++) {
 			directBlocks.add(new DirectBlock(-1));
 		}
 	}
+
 	/**
-	 * get the block index in this array from the real block index
-	 * The Single Indirect Block contains max of 100 direct block, starting from blockIndex 12 up to blockIndex 111
+	 * get the block index in this array from the real block index The Single
+	 * Indirect Block contains max of 100 direct block, starting from blockIndex
+	 * 12 up to blockIndex 111
+	 * 
 	 * @param blockIndex
 	 * @return int the target index in this class
 	 */
-	public int getIndexOfDirectBlock(int actualBlockIndex)
-	{
-		if(actualBlockIndex < 12 || actualBlockIndex > 111)
-		{
-		    System.out.println("block number < 12 or > 111, should not access SIB");
+	public int getIndexOfDirectBlock(int actualBlockIndex) {
+		if (actualBlockIndex < 12) {
+			System.out.println("block number < 12, should not access SIB");
+		} else if (actualBlockIndex >= 112 && actualBlockIndex <= 211) {
+			return actualBlockIndex - 112;
+		} else if (actualBlockIndex > 211) {
+			// calling from double indirect block
+			int minusOffest = actualBlockIndex - 112;
+			int mod100 = minusOffest % 100;
+			return mod100;
 		}
+		// if from 11 ~ 111
 		return actualBlockIndex - 12;
 	}
-	
+
 	/**
-	 * gets the corresponding DB from the list according to the actual block index
-	 * @param actualBlockIndex the actual block index
+	 * gets the corresponding DB from the list according to the actual block
+	 * index
+	 * 
+	 * @param actualBlockIndex
+	 *            the actual block index
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	public DirectBlock getDirectBlockFromBlockIndex(int actualBlockIndex)
-	{
+	public DirectBlock getDirectBlockFromBlockIndex(int actualBlockIndex) {
 		int targetIndex = this.getIndexOfDirectBlock(actualBlockIndex);
 		return directBlocks.get(targetIndex);
 	}
 
 	// setter for the directBlocks arraylist
-    public void setDirectBlockInfo(int index, DirectBlock db)
-    {
-    	this.directBlocks.set(index, db);
-    }
+	public void setDirectBlockInfo(int index, DirectBlock db) {
+		this.directBlocks.set(index, db);
+	}
 }
